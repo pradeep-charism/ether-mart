8ad5223fa92c67b2ea8e792ddcc10eed5c2c1418
pradeep-charism <pradeep.charism@gmail.com>
2019-08-25 01:05:11 +0800

    opening ether mart!

--- src/shop/js/ether-mart.js
+++ src/shop/js/ether-mart.js
@@ -3,7 +3,7 @@ App = {
   contracts: {},
 
   init: async function () {
-    $.getJSON('../shop-items.json', function (data) {
+    $.getJSON('../products/mobile-items.json', function (data) {
       var shopsRow = $('#shopsRow');
       var shopTemplate = $('#shopTemplate');
 
