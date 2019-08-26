7fa415d47d0bd397e2aab6b874ccba16c79c9e38
pradeep-charism <pradeep.charism@gmail.com>
2019-08-26 20:56:14 +0800

    opening ether mart! - sell coins

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,5 @@ const EtherMart = artifacts.require('./EtherMart.sol');
 
 module.exports = async function (deployer, network, accounts) {
     const wallet = accounts[0];
-    await deployer.deploy(
-        EtherMart,
-        wallet
-        );
+    await deployer.deploy(EtherMart, wallet);
  };
